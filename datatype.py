96a54d94e889996a3b1a7184d9db39730afaae9b
KrazyDevPixel <arham.sjws25@gmail.com>
2025-09-02 19:16:25 +0600

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,20 @@
+#DATATYPE
+name="Arham"
+age=8
+is_student=True
+weight=27.6
+print("Name:", name)
+print("Age:", age)
+print("Weight:", weight)
+print("Is Student:", is_student)
+print("Data type of name:", type(name))
+print("Data type of age:", type(age))
+print("Data type of weight:", type(weight))
+print("Data type of is_student:", type(is_student))
+print("After typecasting...")
+print(age)
+age=str(age)
+print("The data type of age after typecasting is:", type(age))
+weight=int(weight)
+print("The data type of weight after typecasting is:", type(weight))
+print(weight)
