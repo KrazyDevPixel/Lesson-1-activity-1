96c9e738678c2fe353dc4b799d3e70003db78216
KrazyDevPixel <arham.sjws25@gmail.com>
2025-09-02 19:52:51 +0600

    Add files via upload

---
+++ afcevenodd.py
@@ -0,0 +1,5 @@
+num=int(input("Enter a odd/even number: "))
+if num%2==0:
+    print(num,"is even")
+else:
+    print(num,"is odd")                                                                                                             
