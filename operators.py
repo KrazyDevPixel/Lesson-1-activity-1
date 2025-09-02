2947697960cb67b2afd9dad21a22fc193b8fcbd0
KrazyDevPixel <arham.sjws25@gmail.com>
2025-09-02 19:26:47 +0600

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,22 @@
+#OPERATORS
+num1=10
+num2=5
+print("The first number is:",num1)
+print("The second number is:",num2)
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Modulus:", num1 % num2)
+print("Exponentiation:", num1 ** num2)
+print("Floor Division:", num1 // num2)
+print("Square of num1:", num1 ** 2)
+print("Square of num2:", num2 ** 2)
+print("SquareRoot of num1:", num1 ** 0.5)
+#LOGICAL OPERATORS
+print("Equal?", num1 == num2)
+print("Not Equal?", num1 != num2)
+print("NUM1 Greater?", num1 > num2)
+print("NUM2 Greater?", num2 > num1)
+result=num1/2+num2**2+10
+print("Result of expression:", result)
