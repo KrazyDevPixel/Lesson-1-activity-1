48a63759d59a0e2d0f560ed30ce44332ce35fe1d
KrazyDevPixel <arham.sjws25@gmail.com>
2025-09-02 19:37:04 +0600

    Add files via upload

---
+++ stringops.py
@@ -0,0 +1,16 @@
+#String Operations
+first_name="Codingal "
+last_name="Education"
+full_name=first_name+last_name
+example="haa"*5
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Full Name:", full_name)
+print("Example:", example)
+#String length
+word="coding"
+print("Word:", word)
+print("Length of Word:", len(word))
+print("First Character:", word[0])
+print("Last Character:", word[5])
+print("Slice:", word[0:3])
