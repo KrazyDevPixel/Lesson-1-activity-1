a649664172050c49728cb98db6fc7d3af2be05aa
KrazyDevPixel <arham.sjws25@gmail.com>
2025-09-02 19:52:33 +0600

    Add files via upload

---
+++ afcnum123swap.py
@@ -0,0 +1,10 @@
+num1 = int(input("1st number before swapping : "))
+num2 = int(input("2nd number before swapping : "))
+num3 = int(input("3rd number before swapping : "))
+t = num1
+num1 = num2
+num2 = num3
+num3 = t
+print("1st number after swapping :", num1)
+print("2nd number after swapping :", num2)
+print("3rd number after swapping :", num3)
