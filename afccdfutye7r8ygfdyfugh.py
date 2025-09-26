c172d17d99bc8f2e0adef533c767a4899b0a3e4a
KrazyDevPixel <arham.sjws25@gmail.com>
2025-09-26 10:13:17 +0600

    Update afccdfutye7r8ygfdyfugh.py

--- afccdfutye7r8ygfdyfugh.py
+++ afccdfutye7r8ygfdyfugh.py
@@ -1,11 +1,9 @@
 # Create a tuple
 my_tuple = (1, 2, 3, "apple", True)
 
-# Convert the tuple to a list using the list() function
 my_list = list(my_tuple)
-
-# Print the original tuple and the new list
 print("Original tuple: ", my_tuple)
 print("Converted list: ", my_list)
 print("Type of original: ", type(my_tuple))
 print("Type of converted:", type(my_list))
+
