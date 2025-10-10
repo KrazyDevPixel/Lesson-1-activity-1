cc033b8f59ce2b0a7c5b210830da8f391ae19828
KrazyDevPixel <arham.sjws25@gmail.com>
2025-10-10 19:28:31 +0600

    Update Filehandleop1.py

--- Filehandleop1.py
+++ Filehandleop1.py
@@ -1,3 +1,18 @@
-file=open('DEMO.txt','r')
+file=open('DEMO.txt')
 print(file.read())
 file.close()
+#open the file in read mode
+file_read=open('DEMO.txt','r')
+print('File in read mode - ')
+print(file_read.read())
+file_read.close()           
+
+file_write=open('DEMO.txt','w')
+file_write.write('File in write mode - ')
+file_write.write("Im arham ")
+file_write.close()       
+
+file_append=open('DEMO.txt','a')
+file_append.write('File in append mode - ')
+file_append.write("Im arham ")
+file_append.close()     
