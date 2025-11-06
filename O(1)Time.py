6998dfc9358758739547551e81930e434667940d
KrazyDevPixel <arham.sjws25@gmail.com>
2025-11-06 19:40:29 +0600

    Add files via upload

---
+++ O(1)Time.py
@@ -0,0 +1,8 @@
+def printnumber(n):
+    iterations=0
+    print("Number of iterations entered by user: ",n)
+    iterations+=1
+    print("total iterations done by code: ",iterations)
+printnumber(10)
+printnumber(20)
+print("\n with any 'n' time taken, our number won't change \n")
