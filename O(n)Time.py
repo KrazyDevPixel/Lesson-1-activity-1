cd77d59e0fc8c7242d1826f6682223fdea6b1190
KrazyDevPixel <arham.sjws25@gmail.com>
2025-11-06 19:45:40 +0600

    Add files via upload

---
+++ O(n)Time.py
@@ -0,0 +1,9 @@
+def Ontime(n):
+    iterations=0
+    for i in range(1,n+1):
+        iterations+=1
+    print("When the 'n' is ",n," total iterations done by code: ",iterations)
+Ontime(10)
+Ontime(20)
+Ontime(42)
+print("With every n taken, Iterations will change accordingly \n")
