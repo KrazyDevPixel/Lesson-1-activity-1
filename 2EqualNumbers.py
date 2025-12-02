ca2fe2806cfcb5da79ec67e2fc59f0502dcc2357
KrazyDevPixel <arham.sjws25@gmail.com>
2025-12-02 19:07:55 +0600

    Add files via upload

---
+++ 2EqualNumbers.py
@@ -0,0 +1,8 @@
+def check(n1,n2):
+    if((n1^n2)!=0):
+        print("Not Equal")
+    else:
+        print("Equal")
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+check(num1,num2)
