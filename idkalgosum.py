aea27260efc9e5a2b65eec08da36c7f18261e0cf
KrazyDevPixel <arham.sjws25@gmail.com>
2026-02-17 19:17:35 +0600

    Add files via upload

---
+++ idkalgosum.py
@@ -0,0 +1,25 @@
+def kadane(a):
+    n=len(a)
+    max_so_far=0
+    max_ending_here=0
+    for i in range(0,n):
+        max_ending_here=max_ending_here+a[i]
+        if (max_ending_here<0):
+            max_ending_here=0
+        if (max_so_far<max_ending_here):
+            max_so_far=max_ending_here
+    return max_so_far
+def maxcircularsum(a):
+    n=len(a)
+    max_kadane=kadane(a)
+    max_wrap=0
+    for i in range(0,n):
+        max_wrap+=a[i]
+        a[i]=-a[i]
+    max_wrap=max_wrap+kadane(a)
+    if max_wrap>max_kadane:
+        return max_wrap
+    else:
+        return max_kadane
+a=[-2,1,-3,5,-1,2,1,-5,4]
+print(maxcircularsum(a))
